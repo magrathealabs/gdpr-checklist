8711caec15234577f9928703acb82c85cb4f06e6
Lauro Gripa Neto <lauro@magrathealabs.com>
2019-11-12 19:31:41 -0300

    Traduzidos itens restantes

--- src/data.js
+++ src/data.js
@@ -55,59 +55,64 @@ export const steps = [
   },
   {
     id: 'accountability-management',
-    title:'Responsabilidade & gerenciamento',
+    title:'Prestação de contas & gerenciamento',
     items: [{
         id: 1,
-        title: 'Sua empresa elegeu um Executivo de Proteção de Dados (DPO)',
+        title: 'Sua empresa elegeu um Data Protection Officer (DPO).',
         role: ['processor', 'controller'],
-        description: 'A DPO is only required in three scenarios: (1) the processing is carried out by a public authority or body, except for courts acting in their judicial capacity; (2) the core activities of the business consist of processing operations which, by virtue of their nature, scope, and/or purposes, require regular and systematic monitoring of data subjects on a large scale, or (3) the core activities of the business consist of processing on a large scale special categories of data (sensitive data) pursuant to Article 9 and personal data relating to criminal convictions or offenses pursuant to Article 10. If a DPO is required, the DPO should have knowledge of GDPR guidelines as well as knowledge about the internal processes that involve personal information.',
+        description: 'Um DPO é necessário apenas em três cenários: (1) o processamento é realizado por uma autoridade ou órgão público, exceto para os tribunais que atuam em sua capacidade judicial; (2) as atividades principais do negócio consistem em operações de processamento que, em virtude de sua natureza, escopo e/ou propósitos, requerem monitoramento regular e sistemático dos titulares de dados em larga escala; ou (3) as atividades principais do negócio consistem no processamento em larga escala de categorias especiais de dados (dados sensíveis) nos termos do artigo 9. o e dados pessoais relacionados a condenações ou ofensas criminais nos termos do artigo 10. Se um DPO for necessário, o DPO também deve ter conhecimento das diretrizes da LGPD, assim como conhecimento sobre os processos internos que envolvem informações pessoais.',
         links: [
-          {href: "https://advisera.com/eugdpracademy/gdpr/designation-of-the-data-protection-officer/",
-           title: 'GDPR Article 37 – Designation of the data protection officer'
+          {
+            href: "https://advisera.com/eugdpracademy/gdpr/designation-of-the-data-protection-officer/",
+            title: 'GDPR Article 37 – Designation of the data protection officer'
           }
         ]
       },
       {
         id: 2,
-        title: 'Create awareness among decision makers about GDPR guidelines',
+        title: 'Sua empresa conscientiza os tomadores de decisão sobre as diretrizes da LGPD.',
         role: ['processor', 'controller'],
-        description: 'Make sure key people and decision makers have up-to-date knowledge about the data protection legislation.',
+        description: 'Certifique que as pessoas-chave e os tomadores de decisão têm conhecimento atualizado sobre a LGPD.',
         links: [
-          {href: "https://advisera.com/eugdpracademy/gdpr/data-protection-by-design-and-by-default/",
-           title: 'GDPR Article 25 – Data protection by design and by default'
+          {
+            href: "https://advisera.com/eugdpracademy/gdpr/data-protection-by-design-and-by-default/",
+            title: 'GDPR Article 25 – Data protection by design and by default'
           }
         ]
       },
       {
         id: 3,
-        title: 'Make sure your technical security is up to date. ',
+        title: 'Sua empresa garante que sua segurança técnica está atualizada.',
         role: ['processor', 'controller'],
-        description: 'For SaaS software companies, use the SaaS CTO security checklist as a starting point below.',
+        description: 'Para empresas de software SaaS, use a lista de verificação de segurança SaaS CTO como ponto de partida abaixo.',
         links: [
-          {title: "SaaS CTO security checklist",
-           href: 'https://cto-security-checklist.sqreen.io/'
+          {
+            title: "SaaS CTO security checklist",
+            href: 'https://cto-security-checklist.sqreen.io/'
           },
-          {href: "https://advisera.com/eugdpracademy/gdpr/data-protection-by-design-and-by-default/",
-           title: 'GDPR Article 25 – Data protection by design and by default'
+          {
+            href: "https://advisera.com/eugdpracademy/gdpr/data-protection-by-design-and-by-default/",
+            title: 'GDPR Article 25 – Data protection by design and by default'
           }
         ]
       },
       {
         id: 4,
-        title: 'Train staff to be aware of data protection',
+        title: 'Sua empresa treina a equipe para conhecer as boas práticas de proteção de dados.',
         role: ['processor'],
-        description: 'A lot of security vulnerabilities involve cooperation of an unwitting person with access to internal systems. Make sure your employees are aware of these risks.',
+        description: 'Muitas vulnerabilidades de segurança envolvem a cooperação inconsciente de uma pessoa com acesso a sistemas internos. Certifique que seus funcionários estão cientes desses riscos.',
         links: [
-          {href: "https://advisera.com/eugdpracademy/gdpr/data-protection-by-design-and-by-default/",
-           title: 'GDPR Article 25 – Data protection by design and by default'
+          {
+            href: "https://advisera.com/eugdpracademy/gdpr/data-protection-by-design-and-by-default/",
+            title: 'GDPR Article 25 – Data protection by design and by default'
           }
         ]
       },
       {
         id: 5,
-        title: 'You have a list of sub-processors and your privacy policy mentions your use of this sub-processor',
+        title: 'Sua empresa têm uma lista de sub-processadores e sua política de privacidade menciona seu uso destes sub-processadores',
         role: ['processor'],
-        description: 'You should inform your customers of the use of any sub-processor. They should consent by accepting your privacy policy.',
+        description: 'Você deve informar seus clientes sobre o uso de qualquer subprocessador. Eles devem consentir ao aceitar sua política de privacidade.',
         links: [
           {
             href: "https://advisera.com/eugdpracademy/gdpr/processor/",
@@ -121,39 +126,43 @@ export const steps = [
       },
       {
         id: 6,
-        title: 'If your business operates outside the EU, you have appointed a representative within the EU.',
+        title: 'Se sua empresa opera fora do Brasil, você nomeou um representante dentro do Brasil.',
         role: ['processor', 'controller'],
-        description: 'If you have a business outside of the EU and you collect data on EU citizens, you should assign a representative in one of the member states for your business. This person should handle all issues related to processing. In particular, a local authority should be able to contact this person.',
+        description: 'Se você tem uma empresa fora do Brasil e coleta dados sobre cidadãos brasileiros, deve designar um representante no país para sua empresa. Essa pessoa deve lidar com todos os problemas relacionados ao processamento. Em particular, uma autoridade local deve poder entrar em contato com essa pessoa.',
         links: [
-          {href: "https://advisera.com/eugdpracademy/gdpr/representatives-of-controllers-or-processors-not-established-in-the-union/",
-           title: 'GDPR Article 27 – Representatives of controllers or processors not established in the Union'
+          {
+            href: "https://advisera.com/eugdpracademy/gdpr/representatives-of-controllers-or-processors-not-established-in-the-union/",
+            title: 'GDPR Article 27 – Representatives of controllers or processors not established in the Union'
           }
         ]
       },{
         id: 7,
-        title: 'You report data breaches involving personal data to the local authority and to the people (data subjects) involved',
+        title: 'Você denuncia violações de dados envolvendo dados pessoais à autoridade local e às pessoas (titulares dos dados) envolvidas.',
         role: ['processor', 'controller'],
-        description: 'Personal data breaches should be reported within 72 hours to the local authority. You should report what data has been lost, what the consequences are and what countermeasures you have taken. Unless the data leaked was encrypted, you should also report the breach to the person (data subject) whose data you lost.',
+        description: 'As violações de dados pessoais devem ser relatadas dentro de 72 horas à autoridade local. Você deve relatar quais dados foram perdidos, quais são as conseqüências e que contramedidas você tomou. A menos que os dados vazados tenham sido criptografados, você também deve denunciar a violação à pessoa (titular dos dados) cujos dados foram perdidos.',
         links: [
-          {href: "https://advisera.com/eugdpracademy/gdpr/notification-of-a-personal-data-breach-to-the-supervisory-authority/",
-           title: 'GDPR Article 33 – Notification of a personal data breach to the supervisory authority'
+          {
+            href: "https://advisera.com/eugdpracademy/gdpr/notification-of-a-personal-data-breach-to-the-supervisory-authority/",
+            title: 'GDPR Article 33 – Notification of a personal data breach to the supervisory authority'
           },
-          {href: "https://advisera.com/eugdpracademy/gdpr/communication-of-a-personal-data-breach-to-the-data-subject/",
-           title: 'GDPR Article 34 – Communication of a personal data breach to the data subject'
+          {
+            href: "https://advisera.com/eugdpracademy/gdpr/communication-of-a-personal-data-breach-to-the-data-subject/",
+            title: 'GDPR Article 34 – Communication of a personal data breach to the data subject'
           }
         ]
       },{
         id: 8,
-        title: 'There is a contract in place with any data processors that you share data with',
+        title: 'Existe um contrato com todos os processadores de dados com os quais você compartilha dados.',
         role: [ 'controller'],
-        description: 'The contract should contain explicit instructions for the storage or processing of data by the processor. The contract should set out the subject matter and duration of the processing, the nature and purpose of the processing, the type of personal data and categories of data subjects and the obligations and rights of the controller. For example, this could include a contract with your hosting provider. The same contract requirements apply when a processor engages a sub-processor to assist it in fulfilling processing activities on behalf of the controller',
+        description: 'O contrato deve conter instruções explícitas para o armazenamento ou processamento de dados pelo processador. O contrato deve definir o objeto e a duração do processamento, a natureza e a finalidade do processamento, o tipo de dados pessoais e categorias de titulares de dados e as obrigações e direitos do controlador. Por exemplo, isso pode incluir um contrato com seu provedor de hospedagem. Os mesmos requisitos de contrato se aplicam quando um processador contrata um subprocessador para ajudá-lo a realizar atividades de processamento em nome do controlador.',
         links: [
           {
             href: "https://advisera.com/eugdpracademy/gdpr/processor/",
             title: 'GDPR Article 28 – Processor'
           },
-          {href: "https://advisera.com/eugdpracademy/gdpr/processing-under-the-authority-of-the-controller-or-processor/",
-           title: 'GDPR Article 29 – Processing under the authority of the controller or processor'
+          {
+            href: "https://advisera.com/eugdpracademy/gdpr/processing-under-the-authority-of-the-controller-or-processor/",
+            title: 'GDPR Article 29 – Processing under the authority of the controller or processor'
           },
           {
             href: "https://www.gdprtracker.io",
